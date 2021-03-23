941edbd0cb1ac9bdacae96ab7e524aa48badc28e
ppasz21 <ppaszkowski21@gmail.com>
2021-03-24 00:43:06 +0100

    PseudoRandomNumberGenerator work started

---
+++ BSK_PPAOKW/PS/PS3/Lfsr.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSK_PPAOKW.PS
+{
+    public class Lfsr
+    {
+        public List<bool[]> TableXOR { get; set; }
+        public int RowLength { get; set; }
+        public List<bool> Result { get; set; }
+        public int[] Key { get; set; }
+
+        public bool IsStopped { get; set; }
+
+        public Lfsr(int rowLength)
+        {
+            RowLength = rowLength;
+            TableXOR = new List<bool[]>();
+            Result = new List<bool>();
+        }
+
+        public bool AddRow()
+        {
+            if (TableXOR.Count == 0)
+            {
+                bool[] row = new bool[RowLength];
+                for (int i = 0; i < RowLength; i++)
+                {
+                    Random random = new Random();
+                    int q = random.Next(0, 2);
+                    if (q == 0)
+                    {
+                        row[i] = false;
+                    }
+                    else
+                    {
+                        row[i] = true;
+                    }
+                }
+                TableXOR.Add(row);
+                bool xor = OperationXOR(row);
+                Result.Add(xor);
+                return xor;
+                
+            }
+            else
+            {
+                bool[] row = new bool[RowLength];
+                row[0] = Result.LastOrDefault();
+                for (int i = 1; i < RowLength; i++)
+                {
+                    row[i] = TableXOR.LastOrDefault()[i];
+                }
+                TableXOR.Add(row);
+                bool xor = OperationXOR(row);
+                Result.Add(xor);
+                return xor;
+            }
+        }
+
+        private bool OperationXOR(bool[] row)
+        {
+            if (row[0] != row[RowLength-1])
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- BSK_PPAOKW/PS/PS3/PS3PseudorandomNumber.xaml.cs
+++ BSK_PPAOKW/PS/PS3/PS3PseudorandomNumber.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace BSK_PPAOKW.PS
 {
@@ -20,9 +11,73 @@ namespace BSK_PPAOKW.PS
     /// </summary>
     public partial class PS3PseudorandomNumber : UserControl
     {
+        public Lfsr LfsrMethod { get; set; }
         public PS3PseudorandomNumber()
         {
             InitializeComponent();
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            if (LfsrMethod != null)
+            {
+                LfsrMethod.IsStopped = false;
+            }
+            GenerateKey();
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            if(LfsrMethod != null)
+            {
+                LfsrMethod.IsStopped = true;
+            }
+        }
+
+        public void GenerateKey()
+        {
+            if(LfsrMethod==null)
+            {
+                return;
+            }
+            while (!LfsrMethod.IsStopped)
+            {
+                bool row = LfsrMethod.AddRow();
+                if (row)
+                {
+                    Result.Text += "1";
+                }
+                else
+                {
+                    Result.Text += "0";
+                }
+                return;
+            }
+        }
+        private void Create_Click(object sender, RoutedEventArgs e)
+        {
+            string polynomial = Polynomial.Text.ToString();
+            int[] Powers = new int[polynomial.Length / 2];
+            int counter = 0;
+            try
+            {
+                for (int i = 0; i < polynomial.Length; i++)
+                {
+                    if (polynomial[i] == '^')
+                    {
+                        Powers[counter] = (Int32.Parse(polynomial[i + 1].ToString()));
+                        counter++;
+                    }
+                }
+                ErrorTextBlock.Text = "";
+                LfsrMethod = new Lfsr(Powers.Max());
+                LfsrMethod.IsStopped = false;
+
+            }
+            catch (Exception)
+            {
+                ErrorTextBlock.Text = "WRONG polynomial!";
+            }
         }
     }
 }
