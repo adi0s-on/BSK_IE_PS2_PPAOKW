5398eff64c0bdbbe2bcc0cd8251eb648417f6b99
kwaszczuk10 <kwaszczuk10@gmail.com>
2021-04-26 22:26:32 +0200

    Cn+Dn=Kn and Pc2

--- BSK_PPAOKW/PS/PS4/DES.cs
+++ BSK_PPAOKW/PS/PS4/DES.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace BSK_PPAOKW.PS
@@ -8,21 +9,29 @@ namespace BSK_PPAOKW.PS
         private byte[] Content { get; set; }
         private byte[] Helper { get; set; }
         private BitArray Key { get; set; }
-        private BitArray KeyLeft { get; set; }
-        private BitArray KeyRight { get; set; }
+        private BitArray[] KeyLeft { get; set; }
+        private BitArray[] KeyRight { get; set; }
+        private BitArray[] Kn { get; set; }
         private List<BitArray> Bits { get; set; }
         public DES(byte[] content)
         {
             Content = content;
             Bits = new List<BitArray>();
             Key = new BitArray(64);
+            KeyLeft = new BitArray[17];
+            KeyRight = new BitArray[17];
+            Kn = new BitArray[16];
         }
         public void Algorythm()
         {
             Input();
             KeyTransformFor56Bits(Key);
             DivideKeyFor28Bits(Key, out BitArray leftKey, out BitArray rightKey);
-            KeyLeft = leftKey; KeyRight = rightKey;
+            KeyMovingIterations(leftKey, rightKey);
+            for (int i = 1; i < 17; i++)
+            {
+                Kn[i-1] = CnDnFusionToKnAndPermutedChoice2(KeyLeft[i], KeyRight[i]);
+            }
             foreach (var block in Bits)
             {
                 InitialPermutation(block);
@@ -110,9 +119,61 @@ namespace BSK_PPAOKW.PS
                 right[i-28] = key[i];
             }
         }
-        private void KeyMovingIterations()
+        private void KeyMovingIterations(BitArray leftKey, BitArray rightKey)
         {
+            KeyLeft[0] = leftKey; KeyRight[0] = rightKey;
+            BitsShifting(KeyLeft);
+            BitsShifting(KeyRight);
+        }
+        private void BitsShifting(BitArray[] bitArrays)
+        {
+            for (int i = 1; i < 17; i++)
+            {
+                bitArrays[i] = bitArrays[i - 1];
+                if (ConstantValues.CnDnBitMove[i - 1] == 1)
+                {
+                    bool bit = bitArrays[i][0];
+                    for (int j = 1; j < 27; j++)
+                    {
+                        bitArrays[i][j - 1] = bitArrays[i][j];
+                    }
+                    bitArrays[i][27] = bit;
+                }
+                else
+                {
+                    bool bit = bitArrays[i][0];
+                    bool bit2 = bitArrays[i][1];
+                    for (int j = 1; j < 27; j++)
+                    {
+                        bitArrays[i][j - 1] = bitArrays[i][j];
+                    }
+                    bitArrays[i][26] = bit;
+                    bitArrays[i][27] = bit2;
+                }
+            }
+        }
 
+        private BitArray CnDnFusionToKnAndPermutedChoice2(BitArray Cn, BitArray Dn)
+        {
+            BitArray Kn = new BitArray(48);
+            for (int i = 0; i < 24; i++)
+            {
+                Kn[i] = Cn[i];
+            }
+            for (int i = 24; i < 48; i++)
+            {
+                Kn[i] = Dn[i-24];
+            }
+            BitArray helper = new BitArray(48);
+            for (int i = 0; i < 48; i++)
+            {
+                helper[i] = Kn[i];
+            }
+            for (int i = 0; i < 48; i++)
+            {
+                Kn[i] = helper[ConstantValues.ExpandingPermutation[i] - 1];
+            }
+            return Kn;
         }
     }
 }
